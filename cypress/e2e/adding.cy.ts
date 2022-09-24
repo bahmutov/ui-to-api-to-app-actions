178d8af93f1e19d27924df926fd475f6b75203a6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-24 08:26:35 -0400

    use todomvc with backend

--- cypress/e2e/adding.cy.ts
+++ cypress/e2e/adding.cy.ts
@@ -1,3 +1,6 @@
 it('adds a new todo', () => {
   cy.visit('/')
+  cy.get('body').should('have.class', 'loaded')
+  const title = `${Cypress.currentTest.title} ${Cypress._.random(1e5)}`
+  cy.get('.new-todo').type(title + '{enter}')
 })
